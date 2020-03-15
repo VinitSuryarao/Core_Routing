f527433fa4e40c91e619c2df9daee4492c28c0aa
Vinit Suryarao <60574327+VinitSuryarao@users.noreply.github.com>
2020-03-15 19:48:09 +0530

    Add files via upload

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Routing.Models;
+
+namespace Routing.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        //// Without Routing
+        //[HttpPost]
+        //public IActionResult OnSubmit()
+        //{
+        //    return View("Index");
+        //}
+
+        //// Virtual URL Partial Route
+        //[HttpPost("fkdjfkdb4783248bjdbf")]
+        //public IActionResult OnSubmit()
+        //{
+        //    return View("Index");
+        //}
+
+        // Actual Routing With Route Define
+        [HttpPost("fkdjfkdb4783248bjdbf", Name ="client_Submit")]
+        public IActionResult OnSubmit()
+        {
+            return View("Index");
+        }
+    }
+}
